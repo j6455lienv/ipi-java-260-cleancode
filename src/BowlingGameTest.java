e4bef733cdb3d827a77eaa3afba6222ff9a3bc2f
Julien Venet <venet.julien@gmail.com>
2018-04-22 16:14:00 +0200

    final-BowlingGame

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
@@ -5,33 +6,50 @@ import static junit.framework.TestCase.assertEquals;
 
 public class BowlingGameTest {
 
-    BowlingGame game = new BowlingGame();
+    private BowlingGame g;
 
-    @Test
-    public void queDesGoutieresDonne0Points() {
-        rollMany(20, 0);
-        assertEquals(0, game.score());
+    void rollMany(int n, int pins, BowlingGame g) {
+        for (int i = 0; i < n; i++) g.roll(pins);
+    }
+
+    @Before
+    public void setUp() {
+        this.g = new BowlingGame();
     }
 
     @Test
-    public void uneSeuleQuille(){
-        game.roll(1);
-        rollMany(19, 0);
-        assertEquals(1, game.score());
+    public void testZero() {
+        rollMany(20, 0, g);
+        assertEquals(0, g.score());
     }
 
     @Test
-    public void bonusDuSpare(){
-        game.roll(8);
-        game.roll(2);
-        game.roll(1);
-        rollMany(17, 0);
-        assertEquals(12, game.score());
+    public void testAllOnes() {
+        rollMany(20, 1, g);
+        assertEquals(20, g.score());
     }
 
-    private void rollMany(int numberRolls, int numberPins) {
-        for (int i = 0; i < numberRolls; i++) {
-            game.roll(numberPins);
-        }
+    @Test
+    public void testOneSpare() {
+        g.roll(5);
+        g.roll(5);
+        g.roll(3);
+        rollMany(17, 0, g);
+        assertEquals(16, g.score());
+    }
+
+    @Test
+    public void testOneStrike() {
+        g.roll(10);
+        g.roll(3);
+        g.roll(4);
+        rollMany(16, 0, g);
+        assertEquals(24, g.score());
+    }
+
+    @Test
+    public void testPerfectGame() {
+        rollMany(12, 10, g);
+        assertEquals(300, g.score());
     }
 }
